5b434a8abb31a1211f37b353807edc428da60095
larkri <kristoffer.larsson@outlook.com>
2023-10-01 23:31:18 +0200

    # Fix Filter!

--- gui.py
+++ gui.py
@@ -1,5 +1,3 @@
-# gui.py
-
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QFileDialog, QLabel, QTextEdit, QComboBox
 from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage
 from PyQt5.QtWebChannel import QWebChannel
@@ -8,8 +6,8 @@ import sys
 import json
 import os
 import datetime
-from event_filter import updateEventTypeComboBox
-from logger import log_exception
+from event_filter import updateEventTypeComboBox, filterData
+from logger import log_info, log_exception
 
 class App(QWidget):
 
@@ -26,6 +24,7 @@ class App(QWidget):
 
         # Lägg till en ComboBox för filtreringsparameter
         self.filterComboBox = QComboBox()
+        self.filterComboBox.currentIndexChanged.connect(self.filterData)
         layout.addWidget(self.filterComboBox)
 
         self.setLayout(layout)
@@ -65,14 +64,21 @@ class App(QWidget):
         return layout
 
     def filterData(self):
-        # Hämta den valda parametern från ComboBox
-        selected_parameter = self.filterComboBox.currentText()
+        try:
+            last_10_events = self.data[-10:]
+            selected_parameter = self.filterComboBox.currentText()
+            self.filtered_data = filterData(last_10_events, selected_parameter)
+            self.sendFilteredDataToMap()
+            self.updateEventTextEdit()
+            log_info(f"Data filtered by {selected_parameter}.")
+        except Exception as e:
+            log_exception(e)
 
-        # Använd filtreringsfunktionen från filter.py för att filtrera datan
-        self.filtered_data = filterData(self.data, selected_parameter)
-
-        # Uppdatera kartan med de filtrerade händelserna
-        self.sendFilteredDataToMap()
+    def updateEventTextEdit(self):
+        event_text = ""
+        for event in self.filtered_data:
+            event_text += f"ID: {event.get('id', 'N/A')}, Type: {event.get('type', 'N/A')}, Location: {event.get('location', 'N/A')}\n"
+        self.eventTextEdit.setText(event_text if event_text else "Ingen matchning.")
 
     def loadJSON(self):
         try:
@@ -83,43 +89,43 @@ class App(QWidget):
                     self.data = json.load(f)
                     event_count = len(self.data)
                     self.label.setText(f"Loaded {event_count} events from the JSON file.")
-
-                    # Uppdatera informationsetiketten om filen
-                    last_modified = datetime.datetime.fromtimestamp(os.path.getmtime(filePath)).strftime(
-                        '%Y-%m-%d %H:%M:%S')
+                    last_modified = datetime.datetime.fromtimestamp(os.path.getmtime(filePath)).strftime('%Y-%m-%d %H:%M:%S')
                     self.fileInfoLabel.setText(f"File: {filePath}\nEvents: {event_count}\nLast Updated: {last_modified}")
-
-                    # Visa de senaste 10 händelserna i QTextEdit
                     last_10_events = self.data[-10:]
                     event_text = ""
                     for event in last_10_events:
                         event_text += f"ID: {event.get('id', 'N/A')}, Type: {event.get('type', 'N/A')}, Location: {event.get('location', 'N/A')}\n"
                     self.eventTextEdit.setText(event_text)
-
-                    # I slutet av loadJSON
                     self.sendLast10EventsToMap()
-
+                    log_info(f"{event_count} events loaded from {filePath}.")
         except Exception as e:
-            # Anropa log_exception för att logga undantaget
             log_exception(e)
 
     def sendLast10EventsToMap(self):
-        last_10_events = self.data[-10:]
-        self.browser.page().runJavaScript(f"updateMap({json.dumps(last_10_events)})")
+        try:
+            last_10_events = self.data[-10:]
+            self.browser.page().runJavaScript(f"updateMap({json.dumps(last_10_events)})")
+            log_info("Sent last 10 events to the map.")
+        except Exception as e:
+            log_exception(e)
 
     def sendFilteredDataToMap(self):
-        # Kontrollera om det finns någon filtrerad data
-        if self.filtered_data:
-            # Skapa en JavaScript-kod för att skicka den filtrerade datan till kartan
-            js_code = f"updateMap({json.dumps(self.filtered_data)})"
-            # Använd runJavaScript-metoden för att köra JavaScript-koden på webbsidan
-            self.browser.page().runJavaScript(js_code)
-        else:
-            # Om det inte finns någon filtrerad data, rensa kartan genom att skicka en tom lista
-            js_code = "clearMap()"
-            self.browser.page().runJavaScript(js_code)
+        try:
+            if self.filtered_data:
+                js_code = f"updateMap({json.dumps(self.filtered_data)})"
+                self.browser.page().runJavaScript(js_code)
+                log_info("Sent filtered data to the map.")
+            else:
+                js_code = "clearMap()"
+                self.browser.page().runJavaScript(js_code)
+                log_info("Cleared the map.")
+        except Exception as e:
+            log_exception(e)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
     sys.exit(app.exec_())
+
+event_filter.py
+
