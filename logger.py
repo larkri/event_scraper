9464d5393a1cdc7227eb267365d8b5f40224db3d
larkri <kristoffer.larsson@outlook.com>
2023-10-01 21:34:48 +0200

    # Backup

---
+++ logger.py
@@ -0,0 +1,13 @@
+# my_logging.py
+import logging
+
+logging.basicConfig(filename='my_app.log', level=logging.DEBUG)
+
+def log_info(message):
+    logging.info(message)
+
+def log_error(message):
+    logging.error(message)
+
+def log_exception(e):
+    logging.error(f"Det uppstod ett fel: {str(e)}", exc_info=True)
